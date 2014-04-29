ddb0fe336379677cf532cb0bf5b1648820bface6
Francesco Cogno <francesco.cogno@outlook.com>
2014-04-29 16:01:33 +0200

    More and less

--- EventTimeNormalizer/Program.cs
+++ EventTimeNormalizer/Program.cs
@@ -11,6 +11,8 @@ namespace EventTimeNormalizer
     class Program
     {
         private static log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+        private static bool fFirstRowHeader = true;
         static void Main(string[] args)
         {
             using (System.IO.Stream s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("EventTimeNormalizer.log4net configuration.xml"))
@@ -25,15 +27,50 @@ namespace EventTimeNormalizer
             WorksheetPart worksheetPart1 = (WorksheetPart)(wp.GetPartById(sheet.Id));
             SheetData sheetData = worksheetPart1.Worksheet.GetFirstChild<SheetData>();
 
+            SharedStringTable sharedStringTable = wp.SharedStringTablePart.SharedStringTable;
 
-            foreach(Row row in sheetData.AsEnumerable())
+            bool fFirst = true;
+            foreach (Row row in sheetData.AsEnumerable())
             {
-                log.Debug(row.ToString());
-
-                foreach(Cell cell in row)
+                if (fFirst && fFirstRowHeader)
                 {
-                    var dataType = cell.DataType;
+                    fFirst = false;
+                    continue;
                 }
+
+                StringBuilder sb = new StringBuilder();
+                StringBuilder sb2 = new StringBuilder();
+
+                foreach (Cell cell in row)
+                {
+                    sb.Append(cell.CellReference.Value + "-");
+
+                    if (cell.DataType != null)
+                    {
+                        if (cell.DataType == CellValues.SharedString)
+                        {
+                            SharedStringItem ssi = sharedStringTable.ChildElements[int.Parse(cell.CellValue.Text)] as SharedStringItem;
+                            sb2.Append(ssi.Text.Text + "-");
+                        }
+                        else if (cell.DataType == CellValues.Date)
+                        {
+                            sb2.Append(cell.CellValue.Text);
+                        }
+                        else
+                        {
+
+                        }
+                    } else
+                    {
+                        DateTime dt = DateTime.FromOADate(double.Parse(cell.CellValue.InnerXml));
+
+                        sb2.Append(cell.CellValue.InnerXml);
+                    }
+
+                }
+
+                log.Debug(sb.ToString());
+                log.Debug(sb2.ToString());
             }
 
 
