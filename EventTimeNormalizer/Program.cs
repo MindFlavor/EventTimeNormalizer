841e8164e239642f9c53656da22b212c24ee0f4e
Francesco Cogno <francesco.cogno@outlook.com>
2014-04-29 14:57:36 +0200

    Evo

--- EventTimeNormalizer/Program.cs
+++ EventTimeNormalizer/Program.cs
@@ -10,17 +10,42 @@ namespace EventTimeNormalizer
 {
     class Program
     {
+        private static log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
         static void Main(string[] args)
         {
+            using (System.IO.Stream s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("EventTimeNormalizer.log4net configuration.xml"))
+            {
+                log4net.Config.XmlConfigurator.Configure(s);
+            }
+            log.DebugFormat("{0:S} v{1:S} started", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name, System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
             SpreadsheetDocument sd = SpreadsheetDocument.Open(@"D:\GIT\EventTimeNormalizer\EventTimeNormalizer\sample\data.xlsx", false);
 
             WorkbookPart wp = sd.Parts.First(item => item.OpenXmlPart is WorkbookPart).OpenXmlPart as WorkbookPart;
+            Sheet sheet = wp.Workbook.Descendants<Sheet>().First();
+            WorksheetPart worksheetPart1 = (WorksheetPart)(wp.GetPartById(sheet.Id));
+            SheetData sheetData = worksheetPart1.Worksheet.GetFirstChild<SheetData>();
 
-            WorksheetPart wps = wp.WorksheetParts.First();
 
-            Worksheet ws = wps.Worksheet;
+            foreach(Row row in sheetData.AsEnumerable())
+            {
+                log.Debug(row.ToString());
 
-           var tt = ws.ToList();
+                foreach(Cell cell in row)
+                {
+                    var dataType = cell.DataType;
+                }
+            }
+
+
+            //WorksheetPart wps = wp.WorksheetParts.First();
+
+            //Worksheet ws = wps.Worksheet;
+
+            //SheetData sheetData = ws.GetFirstChild<SheetData>();
+
+
+
+
 
         }
     }
