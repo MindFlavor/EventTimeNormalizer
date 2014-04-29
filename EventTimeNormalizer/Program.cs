2dc488fe036eabe9d366b1791f1660bfcc2a8fba
Francesco Cogno <francesco.cogno@outlook.com>
2014-04-29 11:43:46 +0200

    Added log4net

--- EventTimeNormalizer/Program.cs
+++ EventTimeNormalizer/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace EventTimeNormalizer
 {
@@ -10,6 +12,16 @@ namespace EventTimeNormalizer
     {
         static void Main(string[] args)
         {
+            SpreadsheetDocument sd = SpreadsheetDocument.Open(@"D:\GIT\EventTimeNormalizer\EventTimeNormalizer\sample\data.xlsx", false);
+
+            WorkbookPart wp = sd.Parts.First(item => item.OpenXmlPart is WorkbookPart).OpenXmlPart as WorkbookPart;
+
+            WorksheetPart wps = wp.WorksheetParts.First();
+
+            Worksheet ws = wps.Worksheet;
+
+           var tt = ws.ToList();
+
         }
     }
 }
